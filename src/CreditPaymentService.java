01754a4e2b7e3f91c42fe3d6e3ff5d0b535bbb53
Redarek <80126370+Redarek@users.noreply.github.com>
2022-06-28 17:18:45 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,10 @@
 public class CreditPaymentService {
     public double calculate(int credit, int duration) {
         double yearMoanRate = 9.99;
-        double monthMoanRate = yearMoanRate/100/12;
+        double monthMoanRate = yearMoanRate / 100 / 12;
         duration *= 12;
         double result = credit * (monthMoanRate / (1 - Math.pow((1 + monthMoanRate), -duration)));
 
-    return result;
+        return result;
     }
 }
